f91cff4b9fd4d521a51506bb4765cd533fcf3fff
Can Soykarafakılı <can.soykarafakili@gmail.com>
2021-06-04 20:05:50 +0200

    GAM Header Bidding Demo project is created

---
+++ GAM Header Bidding Demo/GAM Header Bidding Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GAM Header Bidding Demo
+//
+//  Created by Can Soykarafakili on 04.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
